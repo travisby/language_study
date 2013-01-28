18f826eff447d28b71af9b1dab20e799de67d2db
Travis Beatty <travisby@gmail.com>
2013-01-28 16:00:18 -0500

    Updated WC.hs to handle words instead of lines

--- chapter1/WC.hs
+++ chapter1/WC.hs
@@ -2,4 +2,4 @@
 -- lines beginning with "--" are comments.
 
 main = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+    where wordCount input = show (length (words input)) ++ "\n"
