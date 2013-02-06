d0b317638a33a4ae6c41472a723be17f151ae570
Travis Beatty <travisby@gmail.com>
2013-02-06 16:58:03 -0500

    Added join!

--- chapter3/exercises.hs
+++ chapter3/exercises.hs
@@ -10,3 +10,9 @@ palindrome xs = xs ++ reverse xs
 -- 5 Is a list a palindrome?
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs = xs == reverse xs
+
+-- 7 Create a join function
+join' :: String -> [String] -> String
+join' _ [] = []
+join' _ [x] = x
+join' joiner (x:xs) = x ++ joiner ++ (join' joiner xs)
