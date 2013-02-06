0e420537e48d6644781c60efa1fa12db36921e1e
Travis Beatty <travisby@gmail.com>
2013-02-06 16:47:21 -0500

    Simple isPalindrome function

--- chapter3/exercises.hs
+++ chapter3/exercises.hs
@@ -6,3 +6,7 @@ length' (_:xs) = 1 + length' xs
 -- 4 Turn a list into a palindrome
 palindrome :: [a] -> [a]
 palindrome xs = xs ++ reverse xs
+
+-- 5 Is a list a palindrome?
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
