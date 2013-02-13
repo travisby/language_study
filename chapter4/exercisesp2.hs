80aec9decceff369a9a6d06f6cbd5dc63d6be574
Travis Beatty <travisby@gmail.com>
2013-02-13 07:44:26 -0500

    Added a simple folding solution for asInt

--- chapter4/exercisesp2.hs
+++ chapter4/exercisesp2.hs
@@ -1,7 +1,12 @@
 -- Use a fold (choosing the appropriate fold will make your code much simpler)
 -- to rewrite and improve upon the asInt function from the section called
 -- “Explicit recursion”
+import Data.Char
 asInt_fold :: String -> Int
+asInt_fold xs = foldl asInt_inner 0 xs
+
+-- Used by the fold
+asInt_inner acc x = acc * 10 + digitToInt x
 
 -- How many of the following Prelude functions can you rewrite using list folds? 2 comments
 --
