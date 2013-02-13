39ec9a8cc85f8a678495ddfb020571ae045736c3
Travis Beatty <travisby@gmail.com>
2013-02-13 08:19:33 -0500

    Added some comments for the last part of ch4p2

--- chapter4/exercisesp2.hs
+++ chapter4/exercisesp2.hs
@@ -8,7 +8,9 @@ asInt_fold xs = foldl asInt_inner 0 xs
 -- Used by the fold
 asInt_inner acc x = acc * 10 + digitToInt x
 
--- How many of the following Prelude functions can you rewrite using list folds? 2 comments
---
+-- How many of the following Prelude functions can you rewrite using list folds?
 -- any, cycle, words, unlines
-
+-- any :: (a -> Bool) -> [a] -> Bool
+-- cycle :: [a] -> [a]
+-- words :: String -> [String]
+-- unlines :: [String] -> String
