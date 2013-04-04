dfcae5ec58b98e22922e2d9ea41459cda0838cb5
Travis Beatty <travisby@gmail.com>
2013-04-03 20:39:58 -0400

    Made a sweet statistics library

--- pcd_stats/stats.hs
+++ pcd_stats/stats.hs
@@ -1,3 +1,12 @@
 import Control.Monad
 main = forever $ do
     putStrLn "Yo, please give us some numbas"
+    inputLst <- getLine
+    putStrLn $ doWork inputLst
+
+convertFromStringToListOfNumbas str = map myRead (words str)
+
+myRead x = read x :: Int
+
+doWork str = "The min of the list: " ++ show (minimum ourLst) ++ "\n" ++ "The max of the list: " ++ show (maximum ourLst) ++ "\n"
+   where ourLst = convertFromStringToListOfNumbas str
