89c70a4b42f8b043f2b322d694f4f790b73e4232
Travis Beatty <travisby@gmail.com>
2013-04-03 20:59:02 -0400

    Added the average to our stats function

--- pcd_stats/stats.hs
+++ pcd_stats/stats.hs
@@ -8,5 +8,7 @@ convertFromStringToListOfNumbas str = map myRead (words str)
 
 myRead x = read x :: Int
 
-doWork str = "The min of the list: " ++ show (minimum ourLst) ++ "\n" ++ "The max of the list: " ++ show (maximum ourLst) ++ "\n"
+doWork str = "The min of the list: " ++ show (minimum ourLst) ++ "\n" ++ "The max of the list: " ++ show (maximum ourLst) ++ "\n" ++ "The mean of the list: " ++ show (avg ourLst) ++ "\n"
    where ourLst = convertFromStringToListOfNumbas str
+
+avg xs = sum xs / length xs
